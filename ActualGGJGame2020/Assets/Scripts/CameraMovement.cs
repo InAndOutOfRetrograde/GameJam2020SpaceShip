9aa55c4dfcfbbfb7bf87283d15bdb7382e9b668f
Kyle <kylesmart99@gmail.com>
2020-01-31 22:47:15 -0700

    Camera Movement Works!
    Camera now locks with the player! -Kyle Smart

---
+++ ActualGGJGame2020/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,29 @@
+﻿/* Author: Kyle Smart (Discord: RxBaka#3988)
+ * Date of last modification: 1/31/2020 10:06PM
+ * Description: This code gets the input axis to detect the input from the user and move the player accordingly. Movement is achieved
+ * by adding a force to the players rigidbody2D. By doing this, this gives the game a floaty movement feel, as if the player is in space. 
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    //Vars 
+    [SerializeField]
+    GameObject player;
+    Vector3 offset;
+
+    void Start()
+    {
+        //This gets the difference in position between the camera and the player.
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        //This updates the camera's position based off the players new position.
+        transform.position = player.transform.position + offset;
+    }
+}
