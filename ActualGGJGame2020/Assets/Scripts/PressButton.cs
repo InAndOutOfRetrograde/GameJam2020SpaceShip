d0d16ae7bd183fd4a367c8b2343876c62e4d1654
Bigger_Bag <dumpemail54@gmail.com>
2020-02-01 10:31:21 -0700

    button groundwork

---
+++ ActualGGJGame2020/Assets/Scripts/PressButton.cs
@@ -0,0 +1,36 @@
+﻿//Script to handle a button being pressed by a player via collisions
+//Written by Brandon Collins
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressButton : MonoBehaviour
+{
+
+    //pressed will be true when the button is pressed
+    //False when it has not been pressed
+    bool pressed;
+
+    // initilizes pressed
+    void Start()
+    {
+        pressed = false;
+    }
+    //
+    void onCollisionEnter2D(Collision2D col)
+    {
+        if (!pressed)
+        {
+            pressed = true;
+            var renderer = this.GetComponent<Renderer>();
+            renderer.material.SetColor("_Color", Color.green);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
