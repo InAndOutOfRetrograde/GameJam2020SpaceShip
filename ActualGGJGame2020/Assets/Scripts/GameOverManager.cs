cea6492bc84fba034f3d5f794298e1c3516eaa1e
Kyle <kylesmart99@gmail.com>
2020-02-02 15:13:50 -0700

    Got GameOver call from Screw game!

--- ActualGGJGame2020/Assets/Scripts/GameOverManager.cs
+++ ActualGGJGame2020/Assets/Scripts/GameOverManager.cs
@@ -6,21 +6,27 @@ public class GameOverManager : MonoBehaviour
 {
     [SerializeField]
     GameObject OCGame;
+    [SerializeField]
+    GameObject SGame;
 
     //Vars
     OC_Button OCGameBroken;
+    S_Manager SGameBroken;
+    bool SIsBroken;
     bool OCIsBroken = false;
     // Start is called before the first frame update
     void Start()
     {
         OCGameBroken = OCGame.GetComponent<OC_Button>();
+        SGameBroken = SGame.GetComponent<S_Manager>();
     }
 
     // Update is called once per frame
     void Update()
-    {       
+    {
+        SIsBroken = SGameBroken.Done();
         OCIsBroken = OCGameBroken.GetBrokenCall;
-        if (OCIsBroken)
+        if (OCIsBroken && SIsBroken)
         {
             Debug.Log("Game Over. ");
         }
